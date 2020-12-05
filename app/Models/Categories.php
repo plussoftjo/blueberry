07857ee6c743aad670027176c7692b55e2c09286
unknown <plus.softjo@gmail.com>
2020-12-05 18:46:38 +0200

    Update Translate

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use TCG\Voyager\Traits\Translatable;
 class Categories extends Model
 {
     use HasFactory;
 
     protected $fillable= ['title','description','image'];
+    use Translatable;
+    protected $translatable = ['title','description'];
 }
--- app/Models/Items.php
+++ app/Models/Items.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use TCG\Voyager\Traits\Translatable;
 class Items extends Model
 {
     use HasFactory;
     protected $fillable = ['categories_id','title','description','price','image','discount','stock','new_item','type'];
+    use Translatable;
+    protected $translatable = ['title','description'];
 }
